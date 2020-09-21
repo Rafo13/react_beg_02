9e15a870546d8c8ccefff39205596c778ee29aa1
Rafo13 <rafoarakelyan33gmail.com>
2020-09-21 14:18:44 +0400

    Երկրորդ դասի տնայինը

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import f1 from './demo/mod'
+import   {f2} from './demo/mod'
+import * as obj from './demo/mod'
 
+
+
+
+f1()
+f2()
+console.log(obj.f2())
+console.log(obj.a)
+console.log(obj.b)
+console.log(obj.c)
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/demo/mod.js
@@ -0,0 +1,14 @@
+function f1(){
+   console.log(1000)
+}
+
+export function f2(){
+   console.log(2000)
+}
+
+let a = 10
+let b = 'abcd'
+export let c = 35468
+export {a, b}
+
+export default f1
